b0fad232bd6adb34e46b485756a844948743f0c7
Mithi-code <mithlesh.9m@gmail.com>
2023-07-09 01:07:02 +0530

    code fixes

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,7 +4,7 @@ import CustomInput from "../components/CustomInput";
 import CustomButton from "../components/CustomButtons";
 import Sidebar from "../components/Sidebar";
 import "../styles/Profile.css";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { getProfile, updateProfile } from "../redux/profileReducers";
 import { toast } from "react-toastify";
 
@@ -16,8 +16,6 @@ function Profile() {
   const [patientId, setId] = useState("");
   const [email, setEmail] = useState("");
   const [address, setAddress] = useState("");
-
-  const { role } = useSelector((state) => state.user);
 
   const dispatch = useDispatch();
 
@@ -52,83 +50,79 @@ function Profile() {
 
   return (
     <>
-      {role ? (
-        <div className="profileContainer">
-          <Sidebar />
-          <div className="profileInnerContainer">
-            <div className="profileImage">
-              <img
-                src="https://e1.pxfuel.com/desktop-wallpaper/399/614/desktop-wallpaper-cute-baby-girl-kids-cute-kids-thumbnail.jpg"
-                alt="profile"
-              />
-              <CreateIcon className="icon" />
-            </div>
-            <div className="profileInfo">
-              <CustomInput
-                type="text"
-                placeholder="Name"
-                width="25rem"
-                value={username}
-              />
-              <CustomInput
-                type="text"
-                placeholder="User ID"
-                width="25rem"
-                value={patientId}
-                onChange={(e) => setId(e.target.value)}
-              />
-              <CustomInput
-                type="number"
-                placeholder="age"
-                width="25rem"
-                value={age}
-                onChange={(e) => setAge(e.target.value)}
-              />
+      <div className="profileContainer">
+        <Sidebar />
+        <div className="profileInnerContainer">
+          <div className="profileImage">
+            <img
+              src="https://e1.pxfuel.com/desktop-wallpaper/399/614/desktop-wallpaper-cute-baby-girl-kids-cute-kids-thumbnail.jpg"
+              alt="profile"
+            />
+            <CreateIcon className="icon" />
+          </div>
+          <div className="profileInfo">
+            <CustomInput
+              type="text"
+              placeholder="Name"
+              width="25rem"
+              value={username}
+            />
+            <CustomInput
+              type="text"
+              placeholder="User ID"
+              width="25rem"
+              value={patientId}
+              onChange={(e) => setId(e.target.value)}
+            />
+            <CustomInput
+              type="number"
+              placeholder="age"
+              width="25rem"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            />
 
-              <CustomInput
-                placeholder="Phone Number"
-                type="number"
-                width="25rem"
-                value={phoneNumber}
-                onChange={(e) => setPhoneNumber(e.target.value)}
-              />
-              <CustomInput
-                placeholder="Email"
-                type="type"
-                width="25rem"
-                value={email}
-              />
-              <CustomInput
-                placeholder="Address"
-                type="text"
-                width="25rem"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-              />
-              <div>
-                <select
-                  className="select"
-                  placeholder="Select Gender"
-                  onClick={(e) => setGender(e.target.value)}
-                >
-                  <option disabled selected>
-                    Select your Gender
-                  </option>
-                  <option value="Male">Male</option>
-                  <option value="Female">Female</option>
-                </select>
-              </div>
-              <CustomButton
-                width="27rem"
-                name="Update Profile"
-                onClick={updateProfileInfo}
-              />
+            <CustomInput
+              placeholder="Phone Number"
+              type="number"
+              width="25rem"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+            <CustomInput
+              placeholder="Email"
+              type="type"
+              width="25rem"
+              value={email}
+            />
+            <CustomInput
+              placeholder="Address"
+              type="text"
+              width="25rem"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+            <div>
+              <select
+                className="select"
+                placeholder="Select Gender"
+                onClick={(e) => setGender(e.target.value)}
+              >
+                <option disabled selected>
+                  Select your Gender
+                </option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+              </select>
             </div>
+            <CustomButton
+              width="27rem"
+              name="Update Profile"
+              onClick={updateProfileInfo}
+            />
           </div>
         </div>
-      ) : (
-        <div className="loader">Login First</div>
-      )}
+      </div>
     </>
   );
 }
