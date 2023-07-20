84c7d79994a110fc8cde482e4d31c8f8eefc8756
Mithi-code <mithlesh.9m@gmail.com>
2023-07-20 12:21:02 +0530

    code fixes

--- src/pages/Appointments.js
+++ src/pages/Appointments.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import "react-datepicker/dist/react-datepicker.css";
 import "../styles/Appointments.css";
 import Sidebar from "../components/Sidebar";
 import { toast } from "react-toastify";
